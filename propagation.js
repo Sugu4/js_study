5bf2be4e32e9a879613db7bbf202e09ecfa623a2
Sugu4 <sulo4690@hotmail.com>
2025-12-03 11:20:09 +0100

    EventListener

---
+++ propagation.js
@@ -0,0 +1,18 @@
+const elements = document.querySelectionAll('*')
+
+const div = document.querySelector('div');
+const main = document.querySelector('main');
+const section = document.querySelector('section');
+
+div.addEventListener('click',
+    () => alert(`Bubbling ${div.tagName}`), false
+);
+
+main.addEventListener('click',
+    (e) => {alert(`Bubbling ${main.tagName}`); e.stopPropagation();}, false
+);
+
+section.addEventListener('click',
+    () => alert(`Bubbling ${section.tagName}`), false
+);
+
